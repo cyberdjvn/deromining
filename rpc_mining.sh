f73c76c19cf23fabafa904ea8dfcf82c88a931f3
ThanhMiner <nguyentanthanh@live.com>
2024-06-01 22:02:47 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyv85pdf5pcm4mqfq6xr2qar9su5rvdhpvydegplj8t0es5dk44gvqgsxh87u -r community-pools.mysrv.cloud:10300 -p rpc;
+    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcu7g80ynqjypvzl -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
